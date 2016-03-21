52c36a1c9cda6aee6fe6fecef274270eaf1c1cd4
Kim Youngjin <yachess@gmail.com>
2016-03-22 08:12:38 +0900

    Basic studies

---
+++ guessnum.lisp
@@ -0,0 +1,21 @@
+; Guess the number game
+; Author: Youngjin Kim
+; For education purpose
+;
+
+; Seed random number
+(setf *random-state* (make-random-state t))
+
+(setq *x (random 100))
+(format t "Guess my number(1..99):~%")
+(loop
+    (setq a (read))
+    (cond ( (> a *x)
+                (print "Too great!"))
+          ( (< a *x)
+                (print "Too small!"))
+          ( (eq a *x)
+                (print "Correct!")
+                (return a))
+    )
+)
