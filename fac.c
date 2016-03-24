9024ae90784e99ad99ae6b9019e1fa8f34221c85
Kim Youngjin <yachess@gmail.com>
2016-03-24 09:29:46 +0900

    Minor change

--- fac.c
+++ fac.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-
-int factorial(int n){
-    if (n==0) return 1;
+uint64_t factorial(uint64_t n){
+    if (n<=1) return 1;
     return n * factorial(n-1);
 }
 
 int main(int argc, char** argv){
-    int n = 5;
-    printf("Factorial of %d: %d",n ,factorial(n));
+    int n;
+    if (argc!=2){
+        printf("Usage: %s n\n",argv[0]);
+        return 1;
+    }
+    n = (int)atoi(argv[1]);
+    printf("Factorial of %d: %lu\n",n ,factorial(n));
     return 0;
 }
