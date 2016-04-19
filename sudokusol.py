04a0d253f62ead92daa4d5367d507dd8ec571628
Kim Youngjin <yachess@gmail.com>
2016-04-20 04:17:03 +0900

    slightly better fill() function

--- sudokusol.py
+++ sudokusol.py
@@ -1,9 +1,9 @@
 import sys,random
 
 board = [0 for _ in range(81)]
+
 # precalculate squares of each 9 3x3 box
-bx_sqs = []
-bx_sqs.append([0,1,2,9,10,11,18,19,20])
+bx_sqs=[[0,1,2,9,10,11,18,19,20]]
 bx_sqs.append(map(lambda x: x+3, bx_sqs[0]))
 bx_sqs.append(map(lambda x: x+6, bx_sqs[0]))
 bx_sqs.append(map(lambda x: x+27, bx_sqs[0]))
@@ -20,7 +20,6 @@ def get_avail_sqrs(n,box):
     if box>8:
         return []
     sqs = []
-
     constraints_sq = -1
     for sq in bx_sqs[box]:
         if constraints[sq]==n:
@@ -38,7 +37,6 @@ def get_avail_sqrs(n,box):
             return [constraints_sq]
         else:
             return []
-
     return sqs
 
 def print_board(board):
@@ -46,45 +44,25 @@ def print_board(board):
         print(v),
         if i%9==8: print("")
 
-def fill(n):
+def fill(n,box):
 # Recursively fill the squares in a box with given number
     global board,num_try,occ
     
-    if n==9:      # Done
+    if box==9:
+        box=0
+        n+=1
+    if n==9:
         return True
     num=occ[n][0]
     num_try += 1
     result=False
-    for sq0 in get_avail_sqrs(num,0):
-        board[sq0] = num
-        for sq1 in get_avail_sqrs(num,1):
-            board[sq1] = num
-            for sq2 in get_avail_sqrs(num,2):
-                board[sq2] = num
-                for sq3 in get_avail_sqrs(num,3):
-                    board[sq3] = num
-                    for sq4 in get_avail_sqrs(num,4):
-                        board[sq4] = num
-                        for sq5 in get_avail_sqrs(num,5):
-                            board[sq5] = num
-                            for sq6 in get_avail_sqrs(num,6):
-                                board[sq6] = num
-                                for sq7 in get_avail_sqrs(num,7):
-                                    board[sq7] = num
-                                    for sq8 in get_avail_sqrs(num,8):
-                                        board[sq8] = num
-                                        result = fill(n+1)
-                                        if result:
-                                            return True
-                                        board[sq8] = 0
-                                    board[sq7] = 0
-                                board[sq6] = 0
-                            board[sq5] = 0
-                        board[sq4] = 0
-                    board[sq3] = 0
-                board[sq2] = 0
-            board[sq1] = 0
-        board[sq0] = 0
+    for sq in get_avail_sqrs(num,box):
+        board[sq] = num
+        result = fill(n,box+1)
+        if result:
+            return True
+        else:
+            board[sq] = 0
     return False
 
 if len(sys.argv)!=2:
@@ -102,7 +80,7 @@ if len(constraints)!= 81:
 
 num_try = 0
 #occurence table to sort by frequency
-occ = [[i,0] for i in range(1,10)]
+occ = [[i,0] for i in xrange(1,10)]
 for v in constraints:
     if v!= 0:
         occ[v-1][1]+=1
@@ -110,7 +88,7 @@ for v in constraints:
 occ.sort(key=lambda x: -x[1])
 
 print(occ)
-if fill(0):
+if fill(0,0):
     print("")
     print_board(board)
     print("Found solution after %d trial." % (num_try))
